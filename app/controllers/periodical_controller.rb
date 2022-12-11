8dad8612b7aebc9b525b9967c7ce9b5e3fc1f8f3
kyuuki <kyuuki.japan@gmail.com>
2022-12-11 17:17:05 +0000

    定期ツイート間隔が存在しない場合にエラーになる不具合修正

--- app/controllers/periodical_controller.rb
+++ app/controllers/periodical_controller.rb
@@ -3,12 +3,18 @@ class PeriodicalController < ApplicationController
   # 定期実行の周期 (分)
   #
   def minute
-    @config = Config.find_or_create_by!(key: "periodical.minute")
+    # ↓これだとデータがない場合にバリデーションに引っかかる
+    #@config = Config.find_or_create_by!(key: "periodical.minute")
+    @config = Config.find_or_create_by!(key: "periodical.minute") do |config|
+      config.value = "10"
+    end
   end
 
   def minute_update
     # 作成されてないことはないはずだが念のため
-    @config = Config.find_or_create_by!(key: "periodical.minute")
+    @config = Config.find_or_create_by!(key: "periodical.minute") do |config|
+      config.value = "10"
+    end
 
     if @config.update(params.require(:config).permit(:value))
       redirect_to({ action: :minute }, notice: "更新しました。")
